48a91fa0bf97444e80cb916e3f0068432fb85568
Justin <jhall.auburn@gmail.com>
2019-01-30 13:18:00 -0800

    Initial Commit

---
+++ TapcartWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapcartWeather
+//
+//  Created by Justin Hall on 1/30/19.
+//  Copyright © 2019 jhall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
